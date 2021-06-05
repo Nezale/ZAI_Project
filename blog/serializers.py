b52ec89230a144cf8a859c69767c28d9d7b0e351
Nezale <nezale456@gmail.com>
2021-06-05 02:52:38 +0200

    Next sderializers and views

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,17 +3,24 @@ from .models import Author, Post, PostCategory
 from django.contrib.auth.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['url', 'pk', 'username']
+
+
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='author-detail')
+    user = UserSerializer()
 
     class Meta:
         model = Author
-        fields = ['pk', 'url', 'name', 'nickname', 'firstname', 'lastname', 'birthday', 'about', 'author']
+        fields = ['pk', 'url', 'about', 'user']
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     post_category = serializers.SlugRelatedField(queryset=PostCategory.objects.all(), slug_field='name')
-    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+    author = AuthorSerializer()
 
     class Meta:
         model = Post
--- devops_blog/settings.py
+++ devops_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'blog',
 ]
 
@@ -127,5 +128,22 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+    'blog.custompagination.LimitOffsetPaginationWithUpperBound',
+    'PAGE_SIZE': 2,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
